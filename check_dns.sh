214a41db3860d807ac16c9a1b370d6df7be16c7e
Xi Ning Wang <osswangxining@163.com>
2018-08-04 14:43:14 +0800

    init

---
+++ check_dns.sh
@@ -0,0 +1,5 @@
+host istio-pilot.istio-system
+host istio-pilot.istio-system.svc.cluster.local.
+host istio-ingress.istio-system.svc.cluster.local.
+curl -f 'http://istio-pilot.istio-system:8080/v1/registration'
+
